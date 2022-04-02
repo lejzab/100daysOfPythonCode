70ac0cc95d5a97f8ac5870bf1ca3e0a6c2ca3aad
Paweł Bogaczewicz <0x645@tutanota.com>
2022-04-02 13:58:07 +0200

    who's gonna pay

--- day4/main.py
+++ day4/main.py
@@ -7,10 +7,18 @@ def get_random_number(multiplier: int = 10) -> float:
     return random.random() * multiplier
 
 
+def bor():
+    _map = [['⬛️', '⬛️', '⬛️'], ['⬛️', '⬛️', '⬛️'], ['⬛️', '⬛️', '⬛️']]
+    print(f'{_map[0]}\n{_map[1]}\n{_map[2]}')
+    pos = input('where is do you want to put the treasure? (col, row) ')
+    x, y = pos.split(' ')
+    _map[int(y) - 1][int(x) - 1] = 'X'
+    print(f'{_map[0]}\n{_map[1]}\n{_map[2]}')
+
+
 def main():
-    print(f'ala {get_random_number():.4f}')
+    bor()
 
 
 if __name__ == '__main__':
-    print('no cześć')
     main()
