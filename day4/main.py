41dfcaa1930145df99b50ecede34873d2f9e3112
Paweł Bogaczewicz <0x645@tutanota.com>
2022-04-02 13:10:03 +0200

    day4 on other machine

--- day4/main.py
+++ day4/main.py
@@ -1,10 +1,14 @@
 import random
-import numpy as np
 
 
-def main(multiplier: int = 10):
-    """prints random number"""
-    print(random.random() * multiplier)
+def get_random_number(multiplier: int = 10) -> float:
+    """return random number from [0, multiplier)
+    """
+    return random.random() * multiplier
+
+
+def main():
+    print(f'ala {get_random_number():.4f}')
 
 
 if __name__ == '__main__':
