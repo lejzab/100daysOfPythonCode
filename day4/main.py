753bf238ee5523b624abdbd93774c4162f5f961c
Paweł Bogaczewicz <0x645@tutanota.com>
2022-04-02 12:47:37 +0200

    day 4 start

---
+++ day4/main.py
@@ -0,0 +1,12 @@
+import random
+import numpy as np
+
+
+def main(multiplier: int = 10):
+    """prints random number"""
+    print(random.random() * multiplier)
+
+
+if __name__ == '__main__':
+    print('no cześć')
+    main()
